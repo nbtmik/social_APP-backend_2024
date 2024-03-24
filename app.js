cc3eee7433cdede6a2258666cb364ca6f7f2dd11
Soumik <soumikchakrabarty103@gmail.com>
2024-03-24 14:31:08 +0530

    Initial Commit after completing the server side code

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+require("dotenv").config({path:"config/config.env"});
+
+//Using Middlewares
+app.use(express.json({limit:"50mb"})); // extend limit of 50mb item
+app.use(express.urlencoded({limit:"50mb",extended:true})); // for can use body parser
+app.use(cookieParser());
+app.use(cors({credentials: true,origin:`${process.env.FRONTEND_URL}`}));
+
+//Importing Routes
+const post = require("./routes/post");
+const user = require("./routes/user");
+
+//Using Routes
+app.use("/api/v1",post);
+app.use("/api/v1",user);
+
+
+module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+exports.connectDatabase = () =>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then((c)=>{
+        console.log(`Mongodb connected to: ${c.connection.host}`);
+    })
+    .catch((e)=>{
+        console.log(e);
+    });
+};
